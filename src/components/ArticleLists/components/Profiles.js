d5d9e7146480ba367fec1d7591db6e977f5cc2e0
mbellotti <marianne.bellotti@gmail.com>
2024-03-05 10:09:09 -0500

    profile photo styling

--- src/components/ArticleLists/components/Profiles.js
+++ src/components/ArticleLists/components/Profiles.js
@@ -1,8 +1,8 @@
 export function Profiles(){
     const profiles = ["rasmussen", "halpern", "clark", "dekker"]
-    const p = profiles.map(pro => <a href={"..#/researcher/"+pro}><img src={"../img/"+pro+".jpg"} alt={pro}/></a>)
+    const p = profiles.map(pro =>  <div className="profile-photo"><a href={"..#/researcher/"+pro}><img src={"../img/"+pro+".jpg"} alt={pro} className="img-fluid"/></a></div>)
     return(
-      <div className="profile-photo">
+      <div className="profile-photo-block clearfix">
         {p}
       </div>
     )
